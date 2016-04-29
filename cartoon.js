2570ec94bdaabea423aca4e7e7a0624ba01adb19
Dmitry Homenok <dmitry.homenok@gmail.com>
2016-04-29 20:31:26 +0300

    Add logic to handle vertical screens

--- cartoon.js
+++ cartoon.js
@@ -2,8 +2,12 @@ function setCarouselSize() {
     var wWidth = $(window).width();
     var wHeight = $(window).height();
     
-    if (wHeight > wWidth) {
-        var containerHeight = wHeight * $('.img-cartoon').width() / wWidth;   
+    var originalImageHeight = 1080;
+    var originalImageWidth = 1920;
+    
+    if (wHeight > wWidth) {        
+        var containerHeight = originalImageHeight * wWidth / originalImageWidth;   
+        $('#carousel-example-generic').height(containerHeight);
     } else {
         $('#carousel-example-generic').height($(window).height()-$('#panel-title').height());
     }
